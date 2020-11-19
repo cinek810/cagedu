28584fa55cf3cba96889e82db9b5834511b230f2
Marcin Stolarek <stolarek.marcin@gmail.com>
2020-11-19 16:31:18 +0100

    Don't store full stat, add maxDepth to buildTree operation
    Those are generally to reduce the memory footpring on large filesystem.
    Instead of storing whole stat result we only keep the information useful
    for us.
    Additional argument of maxDepth added to buildTree - limit depth for the
    infromation storred to further lower memory footprint on large
    filesystems.

--- cagedu.py
+++ cagedu.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python
 
-import os, sys, time
+import os, sys, time, datetime
 import logging
 from stat import *
 from anytree import NodeMixin, RenderTree, findall
 from anytree.exporter import DotExporter
 
-logging.basicConfig(level=logging.INFO, format='cdu: %(name)s %(message)s')
+logging.basicConfig(level=logging.DEBUG, format='cdu: %(name)s %(message)s')
 
 class FileStat(NodeMixin):
     def __init__(self, name, stat, parent=None, children=None):
@@ -17,11 +17,14 @@ class FileStat(NodeMixin):
         if children:
             self.children = children
 
-        self.stat = stat
+        self.st_mode = stat.st_mode
+        self.st_mtime = stat.st_mtime
+        self.st_size = stat.st_size
+
 
     def addStats(self, fileSize, fileAge):
         self.totalSize += fileSize
-        self.byteAge = fileSize * fileAge
+        self.byteAge += fileSize * fileAge
         if self.parent:
             logging.debug("Internal adding stats to %s" %  (self.name)) 
             self.parent.addStats(fileSize, fileAge)
@@ -30,43 +33,62 @@ class FileStat(NodeMixin):
 results = dict()
 hierarchy = dict()
 
-def buildTree(top):
+def buildTree(top, maxDepth = 0, currentDepth = 0):
+    logging.debug("currentDepth=%d maxDepth=%d" % (currentDepth, maxDepth))
+    if maxDepth == 0 or currentDepth < maxDepth:
+        for f in os.listdir(top.name):
+            pathname = os.path.join(top.name, f)
+            try:
+                statResult = os.stat(pathname)
+                mode = statResult.st_mode
+            except:
+                logging.debug('Stat failed, skipping %s' % pathname)
+                continue
 
-    for f in os.listdir(top.name):
-        pathname = os.path.join(top.name, f)
-        try:
-            statResult = os.stat(pathname)
-            mode = statResult.st_mode
-        except:
-            logging.debug('Stat failed, skipping %s' % pathname)
-            continue
+            logging.debug('Adding node %s to parent "%s"' % (pathname, top.name))
+            try:
+                newNode = FileStat(pathname.encode('utf-8'),statResult, parent = top)
+            except UnicodeDecodeError:
+                logging.error("Encoding error with name: %s" % (pathname))
+                continue
+            
+            if S_ISDIR(mode):
+                newDepth = currentDepth + 1
+                buildTree(newNode, maxDepth = maxDepth, currentDepth = newDepth)
+    else:
+        logging.debug("Adding stats without new nodes")
+        for path, subdirs, files in os.walk(top.name):
+            for f in files:
+                logging.debug("Joining %s and %s" % (top.name, f));
+                pathname = os.path.join(path, f)
+                try:
+                    statResult = os.stat(pathname)
+                    mode = statResult.st_mode
+                except:
+                    logging.debug('Stat failed, skipping %s' % pathname)
+                    continue
+                logging.debug('Adding stat of %s to node "%s"' % (pathname, top.name))
+                top.addStats(statResult.st_size, statResult.st_mtime)
 
-        logging.debug('Adding node %s to parent "%s"' % (pathname, top.name))
-        try:
-            newNode = FileStat(pathname.encode('utf-8'),statResult, parent = top)
-        except UnicodeDecodeError:
-            logging.error("Encoding error with name: %s" % (pathname))
-            continue
-        
-        if S_ISDIR(mode):
-            buildTree(newNode)
+
 
 def calculateStats(topNode):
-    now = time.time()
 
-    files = findall(topNode, lambda node: S_ISREG(node.stat.st_mode))
+    files = findall(topNode, lambda node: S_ISREG(node.st_mode))
     for fileNode in files:
-        fileSize = fileNode.stat.st_size
-        fileAge = now - fileNode.stat.st_mtime 
+        fileSize = fileNode.st_size
+        fileAge = fileNode.st_mtime 
 
         logging.debug("calculate stat %s in %s" % (fileNode.name, fileNode.parent.name))
-        fileNode.parent.addStats(fileSize, fileAge)
+        fileNode.parent.addStats(fileNode.st_size, fileNode.st_mtime)
 
 def printTree(node):
     for pre, fill, node in RenderTree(node):
-        if S_ISDIR(node.stat.st_mode):
+        if S_ISDIR(node.st_mode):
             try:
-                print("%s %s AveByteAge:%d days Size:%.2fMB" % (pre, node.name, node.byteAge / node.totalSize / 3600 / 24 , node.totalSize / 1024 / 1024 ))
+                timestamp = datetime.datetime.fromtimestamp(node.byteAge / node.totalSize)
+                date = timestamp.strftime('%Y-%m-%d %H:%M:%S')
+                print("%s %s AveDate:%s Size:%.2fMB" % (pre, node.name, date , node.totalSize / 1024 / 1024 ))
             except ZeroDivisionError:
                 print("%s %s (ZERO SIZE)" % (pre, node.name))
 
@@ -81,7 +103,7 @@ if __name__ == '__main__':
     rootNode = FileStat(rootDir,rootStat)
 
     logging.info("Building the tree information");
-    buildTree(rootNode)
+    buildTree(rootNode, maxDepth=3)
 
     logging.info("Processing data structure");
     calculateStats(rootNode)
@@ -89,4 +111,4 @@ if __name__ == '__main__':
     logging.info("Printing the tree");
     printTree(rootNode)
 
-    DotExporter(rootNode).to_picture("/home/cinek/myTree.png");
+    #DotExporter(rootNode, maxlevel=3).to_picture("/home/cinek/myTree.png");
