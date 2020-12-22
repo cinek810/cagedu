0224146272771ced50746f968c8cad15e7581abe
Marcin Stolarek <cinek@schedmd.com>
2020-12-22 17:51:39 +0100

    Split scan and print into different subcommands
    This should allow generation of different reports scanning is
    completed and give ability to generate reports for people without
    access to all subdirectories.

--- cagedu/command_line.py
+++ cagedu/command_line.py
@@ -8,36 +8,84 @@ import sys,os
 import logging
 import argparse
 import time
+import gzip, pickle
 
-def main():
-    parser = argparse.ArgumentParser(description='Disk usage analysis tool.')
-    parser.add_argument('path', metavar='path', type=str, default='.',
+from anytree.exporter import DictExporter
+from anytree.importer import DictImporter
+
+parser = argparse.ArgumentParser(description='Disk usage analysis tool.')
+def parse_options():
+    parser.add_argument('-path', "--path", metavar='path', type=str, default='.',
             help='Path where to start disk usage analysis')
     parser.add_argument("-log", "--log", choices=["DEBUG", "INFO", "ERROR"],
             type=str, default="INFO", metavar='log', help="Log level")
 
     args = parser.parse_args()
-
     logging.basicConfig(level=getattr(logging, str(args.log)),
-            format='cdu: %(name)s %(message)s')
+                format='cdu: %(name)s %(message)s')
 
-    rootDir = args.path
-    try:
-        rootStat = os.stat(rootDir)
-    except:
-        logging.error("Stat on %d failed exiting" % (rootDir))
-        os.exit(1)
+    return args
 
-    rootNode = FileStat(rootDir,rootStat)
+def load_tree(path):
+    importer = DictImporter()
+    with gzip.open(path, 'rb') as f:
+        dataRead = pickle.load(f);
+        logging.info("dataRead="+str(dataRead))
+        rootNode = importer.import_(dataRead)
 
-    logging.info("Building the tree information");
-    start = time.time()
-    cdu.buildTree(rootNode, maxDepth=3)
-    end = time.time()
-    logging.info("\t took:%d seconds, processed:%d files" % (end - start, rootNode.totalFiles))
+    return rootNode
 
-    logging.info("Processing data structure");
-    cdu.calculateStats(rootNode)
+def load_and_process(pathToLoad):
+        start = time.time()
+        logging.info("Loading tree...")
+        rootNode = load_tree(pathToLoad)
+        end = time.time()
+        logging.info("\t took:%d seconds, processed:%d files" % (end - start, rootNode.totalFiles))
 
-    logging.info("Printing the tree");
-    cdu.printTree(rootNode)
+        start = time.time()
+        logging.info("Processing data structure");
+        cdu.calculateStats(rootNode)
+        end = time.time()
+        logging.info("\t took:%d seconds, processed:%d files" % (end - start, rootNode.totalFiles))
+
+
+DEF_BIN_PATH="./.cagedu.gz"
+
+def main():
+
+    if sys.argv[1] == "scan":
+        (sys.argv).remove("scan")
+        parser.add_argument ("-export", "--export", dest='exportFile', type=str, default=DEF_BIN_PATH,
+                help='Path where to store scan result')
+        args = parse_options()
+        rootDir = args.path
+        logging.info("Building the tree information for %s" % (rootDir));
+        try:
+            rootStat = os.stat(rootDir)
+        except:
+            logging.error("Stat on %d failed exiting" % (rootDir))
+            os.exit(1)
+
+        rootNode = FileStat(rootDir,rootStat)
+        start = time.time()
+        cdu.buildTree(rootNode, maxDepth=3)
+        end = time.time()
+        logging.info("\t took:%d seconds, processed:%d files" % (end - start, rootNode.totalFiles))
+
+        with gzip.open(args.exportFile, 'wb') as f:
+            exporter = DictExporter()
+            pickle.dump(exporter.export(rootNode), f, pickle.HIGHEST_PROTOCOL)
+
+    elif sys.argv[1] == "print":
+        (sys.argv).remove("print")
+        parser.add_argument('-import', "--import", dest='importFile', type=str, default=DEF_BIN_PATH,
+                help='Path where to read results of scan');
+        args = parse_options()
+        load_and_process(args.importFile)
+
+        logging.info("Printing the tree");
+        cdu.printTree(rootNode)
+    else:
+        logging.error("No command given, supported sub commands are: scan and print")
+     
+
--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ README = read(str(HERE)+"/README.md")
 # This call to setup() does all the work
 setup(
     name="du-analyzer",
-    version="0.0.1",
+    version="0.0.2",
     description="Read the latest Real Python tutorials",
     long_description=README,
     long_description_content_type="text/markdown",
