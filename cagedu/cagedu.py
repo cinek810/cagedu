9106832a1486fd22a95eff095303ba7626dde03d
Marcin Stolarek <stolarek.marcin@gmail.com>
2021-04-06 12:03:20 +0200

    Logerror when overrinding name in exportDot

--- cagedu/cagedu.py
+++ cagedu/cagedu.py
@@ -155,6 +155,7 @@ def exportWithStyle(node, exportFile):
     index = 0
 
     if node.name == -1:
+        logging.error("Overriding name from -1");
         node.name = hashlib.md5(str(node.filename).encode('utf-8'))
 
     orderedRanks = LevelOrderGroupIter(node, maxlevel=2, filter_ = builtInFilter);
